0f9f77fa5495a14382ff881f6002cd52bbade2ba
Oskar Dufva Sundström <oskar_s_9@hotmail.com>
2023-01-31 13:35:46 +0100

    Update PlayerController.cs
    Added more comments to the PlayerController script

--- GameProject2/Assets/Code/Input/PlayerController.cs
+++ GameProject2/Assets/Code/Input/PlayerController.cs
@@ -6,14 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
+    // Serialized fields for the Rigidbody component of the player and the move speed and jump force values
     [SerializeField] private Rigidbody _characterRB;
     [SerializeField] public float moveSpeed, jumpForce;
 
+    // Private Vector2 field to store the player's movement input
     private Vector2 _move;
 
+    // Timestamp for the last time the player jumped and the minimum interval for jumping
     private float _jumpTimeStamp = 0;
     public float minJumpInterval;
 
+    // Bool to check if the player is grounded
     private bool _isGrounded;
 
 
@@ -24,36 +28,44 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    // Call the MovePlayer method in the Update method
     void Update()
     {
         MovePlayer();
     }
 
 
-    // movement
+    // Method to handle player movement input
     public void OnMove(InputAction.CallbackContext moveContext)
     {
         _move = moveContext.ReadValue<Vector2>();
     }
 
+    // Method to update the player's position based on movement input
     public void MovePlayer()
     {
+        // Calculate the movement vector
         Vector3 movement = new Vector3(_move.x, 0, _move.y);
+
+        // Translate the player's position based on the movement vector
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
 
-        if(movement != Vector3.zero)
+        // If there is movement input, rotate the player towards the movement direction
+        if (movement != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
         }
     }
 
 
-    // jump function
+    // Method to handle player jump input
     public void OnJump(InputAction.CallbackContext jumpContext)
     {
+        // Check if the jump cooldown has ended
         bool jumpCooldownOver = (Time.time - _jumpTimeStamp) >= minJumpInterval;
 
-        if(jumpCooldownOver && _isGrounded)
+        // If the jump cooldown has ended and the player is grounded, jump
+        if (jumpCooldownOver && _isGrounded)
         {
             _jumpTimeStamp = Time.time;
             _characterRB.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
@@ -61,10 +73,11 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Groundcheck
+    // Method to check if the player is touching the ground
     private void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.tag == "Ground")
+        // If the player is touching a collider with the "Ground" tag, set _isGrounded to true
+        if (col.gameObject.tag == "Ground")
         {
             _isGrounded = true;
         }
