9d6ca40e57468f496119e69d57f2aaafa9706c86
Kycklingris <malte.linde.neveling@gmail.com>
2023-02-01 17:37:19 +0100

    upnp maybe works

--- GameProject2/Assets/Code/Scripts/MainMenu/MultiplayerMenuScript.cs
+++ GameProject2/Assets/Code/Scripts/MainMenu/MultiplayerMenuScript.cs
@@ -1,45 +1,59 @@
 using UnityEngine;
 using Mirror;
 using Mirror.Discovery;
+using Mono.Nat;
 
 public class MultiplayerMenuScript : MonoBehaviour
 {
-    [SerializeField] private TMPro.TMP_InputField hostInputField;
-    private NetworkRoomManager networkManager;
-    private NetworkDiscovery networkDiscovery;
+	[SerializeField] private TMPro.TMP_InputField hostInputField;
+	private NetworkRoomManager networkManager;
+	private NetworkDiscovery networkDiscovery;
 
-    private void Awake()
-    {
-        networkManager = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkRoomManager>();
-        networkDiscovery = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkDiscovery>();
-    }
+	private void Awake()
+	{
+		networkManager = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkRoomManager>();
+		networkDiscovery = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkDiscovery>();
+	}
 
-    public void OnHost()
-    {
-        networkManager.maxConnections = 2;
-        networkManager.StartHost();
-        networkDiscovery.AdvertiseServer();
-    }
+	public void OnHost()
+	{
+		NatUtility.DeviceFound += DeviceFound;
+		NatUtility.StartDiscovery();
 
-    public void OnLocal()
-    {
-        networkManager.maxConnections = 1;
-        // TODO: The second local player?
-        networkManager.StartHost();
-    }
+		Debug.Log(NatUtility.IsSearching);
 
-    public void OnJoin()
-    {
-        const string scheme = "kcp";
-        const int port = 7777;
+		// networkManager.maxConnections = 2;
+		// networkManager.StartHost();
+		// networkDiscovery.AdvertiseServer();
+	}
 
-        var host = hostInputField.text;
-        var uriBuilder = new System.UriBuilder();
+	void DeviceFound(object sender, DeviceEventArgs args)
+	{
+		Debug.Log("abow");
+		INatDevice device = args.Device;
+		device.CreatePortMap(new Mapping(Protocol.Udp, 7777, 7777));
+		Debug.Log("pog");
+	}
 
-        uriBuilder.Host = host;
-        uriBuilder.Port = port;
-        uriBuilder.Scheme = scheme;
+	public void OnLocal()
+	{
+		networkManager.maxConnections = 1;
+		// TODO: The second local player?
+		networkManager.StartHost();
+	}
 
-        networkManager.StartClient(uriBuilder.Uri);
-    }
+	public void OnJoin()
+	{
+		const string scheme = "kcp";
+		const int port = 7777;
+
+		var host = hostInputField.text;
+		var uriBuilder = new System.UriBuilder();
+
+		uriBuilder.Host = host;
+		uriBuilder.Port = port;
+		uriBuilder.Scheme = scheme;
+
+		networkManager.StartClient(uriBuilder.Uri);
+	}
 }
