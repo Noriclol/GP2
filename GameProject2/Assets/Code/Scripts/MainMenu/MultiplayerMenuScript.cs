e471e4949cbe1cd8f4d607d8f3a71d5d556ce234
Kycklingris <49489255+Kycklingris@users.noreply.github.com>
2023-02-01 15:19:55 +0100

    Multiplayer Main Menu (#3)
    * basic main menu
    
    * switched to network room manager
    
    * started work on discovery panel

---
+++ GameProject2/Assets/Code/Scripts/MainMenu/MultiplayerMenuScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Mirror;
+
+public class MultiplayerMenuScript : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_InputField hostInputField;
+    private NetworkRoomManager networkManager;
+
+    private void Awake()
+    {
+        networkManager = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkRoomManager>();
+    }
+
+    public void OnHost()
+    {
+        networkManager.maxConnections = 2;
+        networkManager.StartHost();
+    }
+
+    public void OnLocal()
+    {
+        networkManager.maxConnections = 1;
+        // TODO: The second local player?
+        networkManager.StartHost();
+    }
+
+    public void OnJoin()
+    {
+        const string scheme = "kcp";
+        const int port = 7777;
+
+        var host = hostInputField.text;
+        var uriBuilder = new System.UriBuilder();
+
+        uriBuilder.Host = host;
+        uriBuilder.Port = port;
+        uriBuilder.Scheme = scheme;
+
+        networkManager.StartClient(uriBuilder.Uri);
+    }
+}
