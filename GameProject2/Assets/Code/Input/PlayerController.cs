b5185da4912e7d6bccc6a0b8a285010c525a4851
Oskar Dufva Sundström <oskar_s_9@hotmail.com>
2023-02-03 12:11:00 +0100

    Updated playercontroller
    Added facing to mouse position and added a shoot projectiles method.

--- GameProject2/Assets/Code/Input/PlayerController.cs
+++ GameProject2/Assets/Code/Input/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     // Private Vector2 field to store the player's movement input
     private Vector2 _move;
 
+    // Vector2 to store the mouse position
+    private Vector2 _mousePos;
+
     // Timestamp for the last time the player jumped, dodged and the minimum interval for jumping and dodging
     private float _jumpTimeStamp = 0;
     private float _dodgeTimeStamp = 0;
@@ -21,13 +24,14 @@ public class PlayerController : MonoBehaviour
     // Bool to check if the player is grounded
     private bool _isGrounded;
 
-    private PlayerDamage _playerDamage;
-
+    // Projectile 
+    public GameObject projectilePrefab;
+    public Transform projectileSpawnPoint;
+    public float projectileSpeed;
 
     void Awake()
     {
         _characterRB = GetComponent<Rigidbody>();
-        _playerDamage = GetComponent<PlayerDamage>();
     }
 
 
@@ -48,25 +52,54 @@ public class PlayerController : MonoBehaviour
     public void MovePlayer()
     {
         // Calculate the movement vector
-        Vector3 movement = new Vector3(-_move.x, 0, -_move.y);
+        Vector3 movement = new Vector3(_move.x, 0, _move.y);
 
         // Translate the player's position based on the movement vector
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
 
-        // If there is movement input, rotate the player towards the movement direction
-        if (movement != Vector3.zero)
+        //// If there is movement input, rotate the player towards the movement direction
+        //if (movement != Vector3.zero)
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        //}
+    }
+
+    // This method is called whenever a "look" action is performed
+    public void OnLook(InputAction.CallbackContext lookContext)
+    {
+        // Get the mouse position in screen coordinates
+        Vector2 mousePosition = Input.mousePosition;
+
+        // Convert the mouse position to a ray that projects into the scene
+        Ray mouseRay = Camera.main.ScreenPointToRay(mousePosition);
+
+        // Define a plane that intersects with the scene at the object's position
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        float rayDistance;
+
+        // Check if the mouse ray intersects with the ground plane
+        if (groundPlane.Raycast(mouseRay, out rayDistance))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            // Get the point at which the mouse ray intersects with the ground plane
+            Vector3 lookPoint = mouseRay.GetPoint(rayDistance);
+
+            // Make the object look at the look point
+            transform.LookAt(lookPoint);
         }
     }
 
 
     // Method to handle player action input
-    public void OnAction(InputAction.CallbackContext actionContext)
+    public void OnShoot(InputAction.CallbackContext shootContext)
     {
-        _playerDamage.Fire();
-        Debug.Log("Action Input called.");
+        if (shootContext.performed)
+        {
+            Vector3 shootDirection = transform.forward;
+            GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+            newProjectile.GetComponent<Rigidbody>().velocity = shootDirection * projectileSpeed;
 
+            Destroy(newProjectile, 5f);
+        }
     }
 
     // Method to handle player jump input
@@ -98,7 +131,6 @@ public class PlayerController : MonoBehaviour
         }
     }
     
-
     // Method to check if the player is touching the ground
     private void OnCollisionEnter(Collision col)
     {
