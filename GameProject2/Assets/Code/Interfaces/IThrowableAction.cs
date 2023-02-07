88d3cf7d556fd6924b198b9030556af3ee48f493
Kycklingris <49489255+Kycklingris@users.noreply.github.com>
2023-02-07 18:51:53 +0100

    mostly workign (#10)

---
+++ GameProject2/Assets/Code/Interfaces/IThrowableAction.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IThrowableAction
+{
+	public void ThrowAction(ThrowableAction type, float value);
+}
