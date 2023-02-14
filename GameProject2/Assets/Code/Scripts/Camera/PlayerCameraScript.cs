5df3d70933082a9a5d767f4d7f15180809e1c3c0
Kycklingris <49489255+Kycklingris@users.noreply.github.com>
2023-02-14 14:53:55 +0100

    Player camera (#21)
    * player camera
    
    * lose check
    
    ---------
    
    Co-authored-by: Jesse Viitanen <essej.viitanen@gmail.com>
    Former-commit-id: b2f87b16ab75f23ba17deeae054e150e9e6cd407

--- GameProject2/Assets/Code/Scripts/Camera/PlayerCameraScript.cs
+++ GameProject2/Assets/Code/Scripts/Camera/PlayerCameraScript.cs
@@ -9,13 +9,32 @@ public class PlayerCameraScript : NetworkBehaviour
     //public Camera playerCamera;
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtual;
 
+    [SerializeField] private Transform lookTarget;
+
+    private Vector3 playerRelative;
+    private Vector2 camDir;
+    private float camDist = 50;
+
     private void Start()
     {
+        
         if (isLocalPlayer)
         {
             //FindObjectOfType is a "heavy" method
             cinemachineVirtual = CinemachineVirtualCamera.FindObjectOfType<CinemachineVirtualCamera>();
-            cinemachineVirtual.LookAt = this.gameObject.transform;
+            lookTarget = GameObject.Find("Cube").transform;
         }
     }
+
+    private void Update()
+    {
+        if(isLocalPlayer)
+        {
+            playerRelative = gameObject.transform.position - lookTarget.position;
+            camDir = new Vector2(playerRelative.x, playerRelative.z).normalized;
+            cinemachineVirtual.transform.position = lookTarget.position + new Vector3(camDir.x * camDist, 20, camDir.y * camDist);
+            cinemachineVirtual.LookAt = lookTarget;
+        }
+    }
+
 }
