aecd8e38388fd3f7b2e551a6c158ae0a73b5cfc1
Oskar Dufva Sundström <oskar_s_9@hotmail.com>
2023-02-15 14:49:27 +0100

    Energy system
    Energy system for the weapon. Deleted the overheating.
    
    
    Former-commit-id: 060158d7abb721852244b25848d7b6a817baab7f

---
+++ GameProject2/Assets/Code/Scripts/Player Scripts/EnergyManagement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyManagement : MonoBehaviour
+{
+    public float maxEnergy = 100f;
+    public float rechargeSpeed = 10f;
+
+    private float currentEnergy;
+    public Slider energySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentEnergy = maxEnergy; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Recharge();
+        energySlider.value = currentEnergy;
+        
+    }
+
+    private void Recharge(){
+        if(currentEnergy < maxEnergy){
+            currentEnergy += rechargeSpeed * Time.deltaTime;
+            currentEnergy = Mathf.Clamp(currentEnergy, 0f, maxEnergy);
+        }
+    }
+
+    public bool ConsumeEnergy(float amount){
+        if(amount <= currentEnergy){
+            currentEnergy -= amount;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public float GetMaxEnergy(){
+        return maxEnergy;
+    }
+
+    public float GetCurrentEnergy(){
+        return currentEnergy;
+    }
+}
