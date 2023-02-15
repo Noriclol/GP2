fa838feb68a58e49805c311f4ed4adf8370d83d6
Kycklingris <malte.linde.neveling@gmail.com>
2023-02-15 13:27:35 +0100

    GameManager allplayersjoined event
    Former-commit-id: a153cd9766a0edc5c1fb52371a970574558b2a0e

--- GameProject2/Assets/Code/Scripts/Managers/GameManager.cs
+++ GameProject2/Assets/Code/Scripts/Managers/GameManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Mirror;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
+    [SerializeField] private UnityEvent allPlayersFoundCallback;
+
+    private GameObject[] _players;
+
+    public GameObject[] players
+    {
+        get { return _players; }
+    }
+
+    private Coroutine searchForPlayersRoutine;
     // Start is called before the first frame update
     void Start()
     {
-        
+        searchForPlayersRoutine = StartCoroutine(SearchForPlayers());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AllPlayersReadyListener(UnityAction call)
     {
+        if (_players.Length == 2)
+        {
+            call.Invoke();
+        }
+        else
+        {
+            allPlayersFoundCallback.AddListener(call);
+        }
+    }
+
+    IEnumerator SearchForPlayers()
+    {
+        while(true)
+        {
+            var foundPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+            if (foundPlayers.Length == 2)
+            {
+                _players = foundPlayers;
+                allPlayersFoundCallback.Invoke();
+                StopCoroutine(searchForPlayersRoutine);
+            }
+
+            yield return null;
+        }
         
     }
 }
